67f4204051ecd76725e42fe61c19582cbe3ca760
m.dove@live.com <m.dove@live.com>
2019-12-11 14:44:15 -0800

    Added form with word types to enter

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,29 +5,79 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      adj1:'',
+      noun1:'',
+      adverb:'',
+      verb:'',
+      adj2:'',
+      noun2:'',
+    }
   }
+  onSubmitHandler = (event) => {
+    event.preventDefault();
 
+    if (this.state.adjective && this.state.species) {
+      this.props.addPetCallback({
+        adj1: this.state.adj1,
+        noun1: this.state.noun1,
+        adverb: this.state.adverb,
+        verb: this.state.verb,
+        adj2: this.state.adj2,
+        noun2: this.state.adj2,
+      });
+
+      this.setState({
+        name: '',
+        species: '',
+        about: '',
+        images: [],
+        image: '',
+        location: '',
+      });
+    }
+  }
   render() {
 
     return (
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{  }</h3>
 
-        <form className="PlayerSubmissionForm__form" >
+        <form className="PlayerSubmissionForm__form" onSubmit={this.onSubmitHandler}>
 
           <div className="PlayerSubmissionForm__poem-inputs">
 
             {
               // Put your form inputs here... We've put in one below as an example
             }
+            The
             <input
-              placeholder="hm..."
+              placeholder="adjective"
+              type="text" />
+
+            <input
+              placeholder="noun"
+              type="text" />  
+            
+            <input
+              placeholder="adverb"
+              type="text" />
+            <input
+              placeholder="verb"
+              type="text" />
+              the
+              <input
+              placeholder="adjective"
+              type="text" />
+              <input
+              placeholder="noun"
               type="text" />
 
           </div>
 
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn"
+            onClick={this.onSubmitHandler} />
           </div>
         </form>
       </div>
