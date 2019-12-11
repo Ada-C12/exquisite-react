028f57b317b2f4623a22c5bc940d0b689a024887
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-12-11 12:45:10 -0800

    implemented validators

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -10,32 +10,39 @@ class PlayerSubmissionForm extends Component {
     this.state = {
       fields: {},
     }
-
-    // this.validators = {
-    //   subjAdjective: /.+/,
-    //   subjNoun: /.+/,
-    //   adverb: /.+/,
-    //   verb: /.+/,
-    //   objAdjective: /.+/,
-    //   objNoun: /.+/,
-    // }
   }
-
-  // validate = (fieldName) => {
-  //   const value = this.state[fieldName];
-  //   const validation = this.validators[fieldName];
-
-  //   return (validation.test(value));
-  // }
 
   componentDidMount = () => {
     this.initializeFields();
+    this.initializeValidators();
+  }
+
+  validate = (fieldName) => {
+    const value = this.state.fields[fieldName];
+    const validation = this.validators[fieldName];
+
+    return (validation.test(value));
+  }
+
+  initializeValidators = () => {
+    this.validators = {};
+    const formFields = this.props.formFields;
+
+    for (const field of formFields) {
+      if (field.key) {
+        const {key} = field;
+        this.validators[key] = /.+/;
+      } else {
+        this.validators[field] = new RegExp(field);
+      }
+    }
   }
 
   initializeFields = () => {
     let updatedState = {};
+    const formFields = this.props.formFields;
 
-    for (const field of this.props.formFields) {
+    for (const field of formFields) {
       if (field.key) {
         const {key} = field;
         updatedState[key] = '';
@@ -69,12 +76,12 @@ class PlayerSubmissionForm extends Component {
     event.preventDefault();
 
     // returns if there's any invalid inputs
-    // const keys = Object.keys(this.validators);
-    // for (const key of keys) {
-    //   if (!this.validators[key].test(this.state[key])) {
-    //     return;
-    //   }
-    // }
+    const keys = Object.keys(this.validators);
+    for (const key of keys) {
+      if (!this.validators[key].test(this.state.fields[key])) {
+        return;
+      }
+    }
 
     // get the values from state
     const submissionComponentsArray = Object.values({ ...this.state.fields });
@@ -87,14 +94,16 @@ class PlayerSubmissionForm extends Component {
   makeInputCollection = (fields) => {
     const inputCollection = Object.keys(fields).map((field, i) => {
       if ( !/the/i.test(field) 
-        && !/[.?,!]/.test(field) 
-        && !/^a$/i.test(field) ) {
+      && !/[.?,!]/.test(field) 
+      && !/^a$/i.test(field) ) {
+        const fieldValid = this.validate(String( field ));
         return <input 
           key={ i }
           name={ field }
           value={ this.state.fields[field] }
           placeholder={ field }
           onChange={ this.onFieldChange }
+          className={ fieldValid ? 'valid' : 'PlayerSubmissionForm__input--invalid' }
           type="text"
         />;
       } else {
@@ -105,15 +114,7 @@ class PlayerSubmissionForm extends Component {
   }
 
   render() {
-    // const subjAdjectiveValid = this.validate('subjAdjective');
-    // const subjNounValid = this.validate('subjNoun');
-    // const adverbValid = this.validate('adverb');
-    // const verbValid = this.validate('verb');
-    // const objAdjectiveValid = this.validate('objAdjective');
-    // const objNounValid = this.validate('objNoun');
-
     const inputs = this.makeInputCollection(this.state.fields);
-    console.log(inputs);
 
     return (
       <div className="PlayerSubmissionForm" onSubmit={this.onFormSubmit}>
@@ -122,55 +123,7 @@ class PlayerSubmissionForm extends Component {
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
             { inputs }
-
-            {/* The
-            <input
-              name={'subjAdjective'}
-              value={this.state.subjAdjective}
-              onChange={this.onFieldChange}
-              // className={subjAdjectiveValid ? '' : 'PlayerSubmissionForm__input--invalid'}
-              placeholder="adjective"
-              type="text" />
-            <input
-              name={'subjNoun'}
-              value={this.state.subjNoun}
-              onChange={this.onFieldChange}
-              // className={subjNounValid ? '' : 'PlayerSubmissionForm__input--invalid'}
-              placeholder="noun"
-              type="text" />
-            <input
-              name={'adverb'}
-              value={this.state.adverb}
-              onChange={this.onFieldChange}
-              // className={adverbValid ? '' : 'PlayerSubmissionForm__input--invalid'}
-              placeholder="adverb"
-              type="text" />
-            <input
-              name={'verb'}
-              value={this.state.verb}
-              onChange={this.onFieldChange}
-              // className={verbValid ? '' : 'PlayerSubmissionForm__input--invalid'}
-              placeholder="verb"
-              type="text" />
-            the
-            <input
-              name={'objAdjective'}
-              value={this.state.objAdjective}
-              onChange={this.onFieldChange}
-              // className={objAdjectiveValid ? '' : 'PlayerSubmissionForm__input--invalid'}
-              placeholder="adjective"
-              type="text" />
-            <input
-              name={'objNoun'}
-              value={this.state.objNoun}
-              onChange={this.onFieldChange}
-              // className={objNounValid ? '' : 'PlayerSubmissionForm__input--invalid'}
-              placeholder="noun"
-              type="text" />
-            . */}
-
           </div>
 
           <div className="PlayerSubmissionForm__submit">
