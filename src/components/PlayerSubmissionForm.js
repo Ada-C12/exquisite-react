7aeedcf29c03bccae90f4207cdda4f9b9294d2a1
Eve Le <nguyenthao.lee@gmail.com>
2019-12-10 23:21:40 -0800

    Added input fields for PlayerSubmissionForm.js and fixed typo in PlayerSubmissionForm.css

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,6 +5,28 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      adj1: '',
+      noun1: '',
+      adv: '',
+      verb: '',
+      adj2: '',
+      noun2: '',
+    }
+  }
+
+  inputClassStyle = (input) => {
+    const style = (input === "") ? "PlayerSubmissionForm__input--invalid" : "PlayerSubmissionForm__input"
+    return style;
+  }
+
+  onInputChange = (event) => {
+    const updatedState = {};
+    const field = event.target.name;
+    const value = event.target.value;
+
+    updatedState[field] = value;
+    this.setState(updatedState);
   }
 
   render() {
@@ -16,14 +38,56 @@ class PlayerSubmissionForm extends Component {
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
+            The
             <input
-              placeholder="hm..."
-              type="text" />
+              name="adj1"
+              placeholder="adjective"
+              type="text"
+              value={this.state.adj1}
+              onChange={this.onInputChange} 
+              className={this.inputClassStyle(this.state.adj1)}/>
 
+            <input
+              name="noun1"
+              placeholder="noun"
+              type="text"
+              value={this.state.noun1}
+              onChange={this.onInputChange} 
+              className={this.inputClassStyle(this.state.noun1)}/>
+
+            <input
+              name="adv"
+              placeholder="adverb"
+              type="text"
+              value={this.state.adv}
+              onChange={this.onInputChange} 
+              className={this.inputClassStyle(this.state.adv)}/>
+
+            <input
+              name="verb"
+              placeholder="verb"
+              type="text"
+              value={this.state.verb}
+              onChange={this.onInputChange} 
+              className={this.inputClassStyle(this.state.verb)}/>
+
+            the
+
+            <input
+              name="adj2"
+              placeholder="adjective"
+              type="text"
+              value={this.state.adj2}
+              onChange={this.onInputChange} 
+              className={this.inputClassStyle(this.state.adj2)}/>
+
+            <input
+              name="noun2"
+              placeholder="noun"
+              type="text"
+              value={this.state.noun2}
+              onChange={this.onInputChange} 
+              className={this.inputClassStyle(this.state.noun2)}/>
           </div>
 
           <div className="PlayerSubmissionForm__submit">
