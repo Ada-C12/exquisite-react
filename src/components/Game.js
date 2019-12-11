1ecea07c8763becf745e62d5c6ff78064907bcb6
geli-gel <angele.zam@gmail.com>
2019-12-11 15:12:32 -0800

    form shows up and sets state on input change

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,14 +3,34 @@ import './Game.css';
 import PlayerSubmissionForm from './PlayerSubmissionForm';
 import FinalPoem from './FinalPoem';
 import RecentSubmission from './RecentSubmission';
+import { valueToNode } from '@babel/types';
 
 class Game extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      submissions: [],
+    }
+  }
+
+  addSubmission = (submission) => {
+    // submission comes in as object
+    // turn submission into submissionString
+
+    const submissionString = `The ${submission.adj1} ${submission.noun} ${submission.adverb} ${submission.verb} the ${submission.adjective} ${submission.noun} .`
+
+    this.setState({
+      submissions: this.state.submissions.push(submissionString)
+    })
   }
 
   render() {
+
+    console.log('this.state', this.State)
+
+    const currentPlayerNumber = this.state.submissions.length + 1
 
     const exampleFormat = FIELDS.map((field) => {
       if (field.key) {
@@ -19,6 +39,8 @@ class Game extends Component {
         return field;
       }
     }).join(" ");
+
+    // console.log('fieldsAndPlaceholders', fieldsAndPlaceholders)
 
     return (
       <div className="Game">
@@ -32,9 +54,14 @@ class Game extends Component {
           { exampleFormat }
         </p>
 
-        <RecentSubmission />
+        <RecentSubmission
+          submissionLine={this.state.submissions[this.state.submissions.length - 1]}
+        />
 
-        <PlayerSubmissionForm />
+        <PlayerSubmissionForm 
+          playerNumber={currentPlayerNumber}  
+          formatFieldsPlaceholders={FIELDS}
+        />
 
         <FinalPoem />
 
--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,29 +5,72 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+    }
   }
 
+  onInputChange = (event) => {
+    const field = event.target.name;
+    const value = event.target.value;
+
+    const updatedState = {};
+
+    updatedState[field] = value;
+
+    this.setState(
+      updatedState
+    )
+  }
+  
+  
   render() {
+    
+    // Make an array of words + keys and placeholders with jsx and display it below
+    
+    const formInputs = this.props.formatFieldsPlaceholders.map((field,i) => {
+      if (field.key) {
+        return (
+          <input 
+            key={i}
+            name={field.key}
+            placeholder={field.placeholder}
+            type="text"
+            onChange={this.onInputChange}
+          />
+        )}
+      else {
+        return field
+      }
+    });
+
+
+    console.log('props of playerSumbissionForm', this.props)
+    console.log('formInputs', formInputs)
+    console.log('this.state', this.state)
+    
 
     return (
       <div className="PlayerSubmissionForm">
-        <h3>Player Submission Form for Player #{  }</h3>
+        <h3>Player Submission Form for Player #{ this.props.playerNumber }</h3>
 
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
 
             {
-              // Put your form inputs here... We've put in one below as an example
+              formInputs // Put your form inputs here... We've put in one below as an example
             }
-            <input
-              placeholder="hm..."
-              type="text" />
-
+       
           </div>
 
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input 
+              type="submit" 
+              value="Submit Line" 
+              className="PlayerSubmissionForm__submit-btn" 
+              // onClick={ }
+            />
           </div>
         </form>
       </div>
