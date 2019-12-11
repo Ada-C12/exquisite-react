6a3279a0a4e30ef6ca81284f2a4dd8c409da9d03
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-11 12:55:24 -0800

    the most recent submission only shows if there is a recent submission

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -4,8 +4,12 @@ import './RecentSubmission.css';
 const RecentSubmission = (props) => {
   return (
     <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      {props.lastLine && (
+      <>
+        <h3>The Most Recent Submission</h3>
+        <p className="RecentSubmission__submission">{props.lastLine}</p>
+      </>
+      )}
     </div>
   );
 }
