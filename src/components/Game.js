c5449aa01e05706f5ef0ade3257205228e728cd3
v <vanessareigosa@gmail.com>
2019-12-10 21:30:08 -0800

    set up relationship between Game and PlayerSubmissionForm to add new sentences to the collection upon form submission.

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,10 +8,23 @@ class Game extends Component {
 
   constructor(props) {
     super(props);
+    //in this state we can store the final poem 
+    this.state = {
+      submittedPoem: []
+    }
+  }
+
+  addSentence = (newSentence) => {
+    //Right now this is adding each submission object to the submitted Poems array. Maybe in here we could destructure it to only add the values that we want?
+    const {submittedPoem} = this.state;
+    submittedPoem.push(newSentence); 
+    this.setState({
+      submittedPoem,
+    })
+    console.log(submittedPoem)
   }
 
   render() {
-
     const exampleFormat = FIELDS.map((field) => {
       if (field.key) {
         return field.placeholder;
@@ -34,7 +47,7 @@ class Game extends Component {
 
         <RecentSubmission />
 
-        <PlayerSubmissionForm />
+        <PlayerSubmissionForm addSentenceCallback= {this.addSentence}/>
 
         <FinalPoem />
 
--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,29 +1,137 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './PlayerSubmissionForm.css';
 
 class PlayerSubmissionForm extends Component {
-
   constructor(props) {
     super(props);
+    //it is in here that we will store the words of the newest submission 
+    this.state = {
+      adjective: '',
+      noun: '',
+      adverb: '',
+      verb: '',
+      adjective2: '',
+      noun2: '',
+    }
   }
 
+  adjectiveValid = () => {
+    return this.state.adjective.match(/[A-z]/)
+  }
+
+  nounValid = () => {
+    return this.state.noun.match(/[A-z]/)
+  }
+  
+  adverbValid = () => {
+    return this.state.adverb.match(/[A-z]/)
+  }
+
+  verbValid = () => {
+    return this.state.verb.match(/[A-z]/)
+  }
+
+  adjective2Valid = () => {
+    return this.state.adjective2.match(/[A-z]/)
+  }
+
+  noun2Valid = () => {
+    return this.state.noun2.match(/[A-z]/)
+  }
+
+  onFormChange = (event) => {
+    const updatedState = {};
+
+    const field = event.target.name;
+    const value= event.target.value;
+
+    updatedState[field] = value;
+
+    this.setState(updatedState)
+  }
+
+
+  onSubmitSentence = (event) => {
+    event.preventDefault();
+    const sentence = {
+      adjective: this.state.adjective,
+      noun: this.state.noun,
+      adverb: this.state.adverb,
+      verb: this.state.verb,
+      adjective2: this.state.adjective2,
+      noun2: this.state.noun2,
+    }
+
+    this.props.addSentenceCallback(sentence);
+    this.setState({
+      adjective: '',
+      noun: '',
+      adverb: '',
+      verb: '',
+      adjective2: '',
+      noun2: '',
+    });
+  }
   render() {
 
     return (
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{  }</h3>
 
-        <form className="PlayerSubmissionForm__form" >
-
+        <form onSubmit={this.onSubmitSentence} className="PlayerSubmissionForm__form" >
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
-            <input
-              placeholder="hm..."
-              type="text" />
-
+            the
+            <input 
+              name="adjective"
+              placeholder="adjective"
+              type="text" 
+              className={this.adjectiveValid() ? "PlayerSubmissionForm__input" : "PlayerSubmissionForm__input--invalid"} 
+              onChange={this.onFormChange} 
+              value={this.state.adjective} 
+            />
+            <input 
+              name="noun"
+              placeholder="noun"
+              type="text" 
+              className={this.nounValid() ? "PlayerSubmissionForm__input" : "PlayerSubmissionForm__input--invalid"} 
+              onChange={this.onFormChange} 
+              value={this.state.noun} 
+            />
+            <input 
+              name="adverb"
+              placeholder="adverb"
+              type="text" 
+              className={this.adverbValid() ? "PlayerSubmissionForm__input" : "PlayerSubmissionForm__input--invalid"} 
+              onChange={this.onFormChange} 
+              value={this.state.adverb} 
+            />
+            <input 
+              name="verb"
+              placeholder="verb"
+              type="text" 
+              className={this.verbValid() ? "PlayerSubmissionForm__input" : "PlayerSubmissionForm__input--invalid"} 
+              onChange={this.onFormChange} 
+              value={this.state.verb} 
+            />
+            the 
+            <input 
+              name="adjective2"
+              placeholder="adjective"
+              type="text" 
+              className={this.adjective2Valid() ? "PlayerSubmissionForm__input" : "PlayerSubmissionForm__input--invalid"} 
+              onChange={this.onFormChange} 
+              value={this.state.adjective2} 
+            />
+            <input 
+              name="noun2"
+              placeholder="noun"
+              type="text" 
+              className={this.noun2Valid() ? "PlayerSubmissionForm__input" : "PlayerSubmissionForm__input--invalid"} 
+              onChange={this.onFormChange} 
+              value={this.state.noun2} 
+            />
+            .
           </div>
 
           <div className="PlayerSubmissionForm__submit">
@@ -33,6 +141,10 @@ class PlayerSubmissionForm extends Component {
       </div>
     );
   }
+}
+
+PlayerSubmissionForm.propTypes = {
+  addSentenceCallback: PropTypes.func.isRequired,
 }
 
 export default PlayerSubmissionForm;
