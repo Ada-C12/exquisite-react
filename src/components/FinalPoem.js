c0810f8522c8b1df32e2e0a394c79b77a55d0a26
Nicky Choi <nickyjinchoi@gmail.com>
2019-12-11 15:49:00 -0800

    Added variables to display all content or final poem

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import './FinalPoem.css';
 
+
+const displayPoem = (poem) => poem.map(function(line, key) {
+    return (
+      'The',
+      <span key={key}>
+        {line}
+        <br/>
+        <br/>
+      </span>
+    )
+})
+
 const FinalPoem = (props) => {
 
-  return (
-    <div className="FinalPoem">
+  const revealPoem = () => {
+    return (
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
+          {displayPoem(props.poem)}
 
       </section>
+    )
+  }
 
+  const serveRevealButton = () => {
+    return (
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={finalRevealHandler}/>
       </div>
+    )
+  }
+
+  const finalRevealHandler = () => {
+    console.log("final reveal!")
+    props.revealFinalCallback()
+  }
+
+  console.log(props.poem)
+  return (
+    <div className="FinalPoem">
+      {props.poem === false ? serveRevealButton() : revealPoem()}
     </div>
   );
 }
