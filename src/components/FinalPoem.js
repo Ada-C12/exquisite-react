95455f65c3b01e9aa5b74510b10953d1b4afd3a5
Monick <monick.keo@gmail.com>
2019-12-12 00:01:46 -0800

    added finalizePoem function in finalPoem component

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,15 +3,23 @@ import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
+  const finalizePoem = () => {
+    this.props.poemLines.map((line, i) => {
+      return (
+        <p key={i}>{line}</p>
+      )
+    })
+  }
+
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-
+        {finalizePoemg}
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={finalizePoem} />
       </div>
     </div>
   );
