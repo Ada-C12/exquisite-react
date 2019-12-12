cf75f04b9bb7148868e2cf9e2e7f2e23e221f54c
Sara Shah Baig <sarashahbaig@gmail.com>
2019-12-11 17:54:04 -0800

    final poem mapped

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import './FinalPoem.css';
+import React from "react";
+import "./FinalPoem.css";
 
-const FinalPoem = (props) => {
-
+const FinalPoem = props => {
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-
+        {props.finalPoem.map(para => (
+          <p>{para}</p>
+        ))}
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input
+          type="button"
+          value="We are finished: Reveal the Poem"
+          className="FinalPoem__reveal-btn"
+        />
       </div>
     </div>
   );
-}
+};
 
 export default FinalPoem;
--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,33 +1,104 @@
-import React, { Component } from 'react';
-import './PlayerSubmissionForm.css';
+import React, { Component } from "react";
+import "./PlayerSubmissionForm.css";
 
 class PlayerSubmissionForm extends Component {
-
   constructor(props) {
     super(props);
+
+    // 
+    this.state = {
+      // playerNumber: 1,
+      adj1: "",
+      noun1: "",
+      adv: "",
+      verb: "",
+      adj2: "",
+      noun2: ""
+    };
   }
 
-  render() {
+  handleSubmit = e => {
+    e.preventDefault();
+    // this.setState({
+    //
+    // });
+    this.props.handleRecentSubmission(this.state);
+  };
 
+  handleChange = event => {
+    let name = event.target.name;
+    let value = event.target.value;
+    // console.log(name, value);
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
     return (
       <div className="PlayerSubmissionForm">
-        <h3>Player Submission Form for Player #{  }</h3>
-
-        <form className="PlayerSubmissionForm__form" >
-
+        <h3>Player Submission Form for Player #{this.props.playerNumber}</h3>
+        <h1>{this.props.propTest}</h1>
+        <form
+          className="PlayerSubmissionForm__form"
+          onSubmit={this.handleSubmit}
+        >
           <div className="PlayerSubmissionForm__poem-inputs">
-
             {
               // Put your form inputs here... We've put in one below as an example
             }
             <input
-              placeholder="hm..."
-              type="text" />
+              placeholder="adjective"
+              type="text"
+              name="adj1"
+              value={this.state.adj1}
+              onChange={this.handleChange}
+            />
+            <input
+              placeholder="noun"
+              type="text"
+              name="noun1"
+              value={this.state.noun1}
+              onChange={this.handleChange}
+            />
 
+            <input
+              placeholder="adverb"
+              type="text"
+              name="adv"
+              value={this.state.adv}
+              onChange={this.handleChange}
+            />
+            <input
+              placeholder="verb"
+              type="text"
+              name="verb"
+              value={this.state.verb}
+              onChange={this.handleChange}
+            />
+            <input
+              placeholder="adjective"
+              type="text"
+              name="adj2"
+              value={this.state.adj2}
+              onChange={this.handleChange}
+            />
+
+            <input
+              placeholder="noun"
+              type="text"
+              name="noun2"
+              value={this.state.noun2}
+              onChange={this.handleChange}
+            />
           </div>
 
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input
+              type="submit"
+              value="Submit Line"
+              className="PlayerSubmissionForm__submit-btn"
+            />
           </div>
         </form>
       </div>
