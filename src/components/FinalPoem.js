84803fac253c1b1dcaf096ad7d3247404f62f6b6
Brianna Kemp <kemp.bri@gmail.com>
2019-12-12 23:51:44 -0800

    Added final poem method to print each line individually

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './FinalPoem.css';
 
+
+const getPoemLines = (poem) => {
+  return poem.map((line) => {
+    return <p>{line}</p>
+  })
+}
+
 const FinalPoem = (props) => {
-
-  return (
-    <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-
-      </section>
-
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+  console.log(props.buttonCheck);
+  
+    return (
+      <div className="FinalPoem">
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+          {getPoemLines(props.poem)}
+        </section>
       </div>
-    </div>
-  );
+    )
 }
 
 export default FinalPoem;
