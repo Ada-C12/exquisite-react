2ef42f87ad479a2b016e5bf7155face57520280c
Eve Le <nguyenthao.lee@gmail.com>
2019-12-13 00:09:59 -0800

    Simplified FinalPoem to be functional component

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import './FinalPoem.css';
 
-class FinalPoem extends React.Component {
-  onButtonClick = () => {
-    this.props.onClickCallBack();
+const FinalPoem = (props) => {
+  const onButtonClick = () => {
+    props.onClickCallBack();
   }
 
-  render() {
-    const allPoems = this.props.allSubmissions.map((poem, i) => {
-      return (
-        <section key={i}>
-          <p>{poem}</p>
-        </section>);
-    });
-
-    const gameResult = (this.props.gameIsOver) ?
-      (<section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-        {allPoems}
-      </section>) :
-      (<div className="FinalPoem__reveal-btn-container">
-        <input
-          onClick={this.onButtonClick}
-          type="button"
-          value="We are finished: Reveal the Poem"
-          className="FinalPoem__reveal-btn" />
-      </div>);
-
+  const allPoems = props.allSubmissions.map((poem, i) => {
     return (
-      <div className="FinalPoem">
-        {gameResult}
-      </div>
-    );
-  }
+      <section key={i}>
+        <p>{poem}</p>
+      </section>);
+  });
+
+  const gameResult = (props.gameIsOver) ?
+    (<section className="FinalPoem__poem">
+      <h3>Final Poem</h3>
+      {allPoems}
+    </section>) :
+    (<div className="FinalPoem__reveal-btn-container">
+      <input
+        onClick={onButtonClick}
+        type="button"
+        value="We are finished: Reveal the Poem"
+        className="FinalPoem__reveal-btn" />
+    </div>);
+
+  return (
+    <div className="FinalPoem">
+      {gameResult}
+    </div>
+  );
 }
 
 export default FinalPoem;
