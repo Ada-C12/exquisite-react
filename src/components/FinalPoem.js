139d389f7aa29b68a4ad8837e627bcbbbc105d9b
Hallie <idhallie@gmail.com>
2019-12-12 20:52:46 -0800

    Finished button disappears upon displaying full poem.

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -4,10 +4,22 @@ import './FinalPoem.css';
 const FinalPoem = ({allLines, showPoem, onClickShowPoemCallback}) => {
 
 const fullPoem = () => {
-  if (showPoem === true) {
+  if (showPoem) {
     return allLines.map(line => <p>{line}</p>)
   };
 } 
+
+const button = () => (
+  <div>
+  { !showPoem && (
+    <div className="FinalPoem__reveal-btn-container">
+    <input onClick={() => { onClickShowPoemCallback() }} type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+  </div>
+    )
+  }
+  </div>
+);
+
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
@@ -15,9 +27,10 @@ const fullPoem = () => {
         {fullPoem()}
       </section>
 
-      <div className="FinalPoem__reveal-btn-container">
+      {button()}
+      {/* <div className="FinalPoem__reveal-btn-container">
         <input onClick={() => { onClickShowPoemCallback() }} type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
-      </div>
+      </div> */}
     </div>
   );
 }
