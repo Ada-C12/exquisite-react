e89a1599cab24d795c91b634a35790d38aa14263
v <vanessareigosa@gmail.com>
2019-12-12 22:21:57 -0800

    refactors

--- src/components/Game.js
+++ src/components/Game.js
@@ -47,7 +47,7 @@ class Game extends Component {
     }
 
     const displayForm = () => {
-      return <PlayerSubmissionForm addSentenceCallback={this.addSentence} player={this.state.player}/>
+      return <PlayerSubmissionForm addSentenceCallback={this.addSentence} player={this.state.player} fields={FIELDS}/>
     }
 
     const onFinalPoemSubmit = () => { 
