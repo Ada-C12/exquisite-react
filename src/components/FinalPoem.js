578be97039d37203500b21e793e88d89a3a64d3b
daniela sanchez <dnsanche@gmail.com>
2019-12-13 11:36:17 -0800

    Reveal Poem on Click

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './FinalPoem.css';
 
-const FinalPoem = (props) => {    
-  
-  const submissionstCollection = props.poems.map((submission, i) => {
-  return <p key={i}>{`The ${submission.adjective} ${submission.noun} ${submission.adverb} ${submission.verb} the ${submission.adjective2} ${submission.noun2}.`}</p>;
-    }
-  );
+// const FinalPoem = (props) => {   
+class FinalPoem extends Component {
+ 
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-        <ul>
-          {submissionstCollection}
-        </ul>
-      </section>
+    this.state = {
+      show: false,
+    };
+  };
 
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+  onShowPoem = () => {
+    this.setState( { show: true } )
+  };
+
+  submissionstCollection () { 
+    const submissionstCollection = this.props.poems.map((submission, i) => {
+      return <p key={i}>{`The ${submission.adjective} ${submission.noun} ${submission.adverb} ${submission.verb} the ${submission.adjective2} ${submission.noun2}.`}</p>; 
+      }
+    );
+    return submissionstCollection
+  }
+
+  render () {
+
+    if (this.state.show === true) {
+
+    return (
+      <div className="FinalPoem">
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+          <ul>
+            {this.submissionstCollection()}
+          </ul>
+        </section>
       </div>
-    </div>
-  );
-}
+    );
+  }
+
+  else {
+    return (
+      <div className="FinalPoem__reveal-btn-container">
+        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={this.onShowPoem} />
+      </div>
+    );
+    } 
+  } 
+};
 
 export default FinalPoem;
