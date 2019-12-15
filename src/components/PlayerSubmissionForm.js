668b1af77a0c571628119a3a75d27e052835ebd1
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-12-15 14:37:00 -0800

    Refactoring input fields to incorporate pattern we learned in class. Now fields reset after submit button is pressed.

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -7,7 +7,7 @@ class PlayerSubmissionForm extends Component {
     super(props);
     this.playerNumber = 1;
     this.state = {
-      // user words are stored in state until submitted
+      // user-inputted words are stored in state until submitted
       article1: 'The',
       adjective1: '',
       noun1: '', 
@@ -20,25 +20,33 @@ class PlayerSubmissionForm extends Component {
     this.addLine = props.addLine;
   }
 
-updateWord = (word, event) => {
+updateWord = (event) => {
   let value = event.target.value
   this.setState({
-    [word]: value
+    [event.target.name]: value
   })
 }
 
 onSubmitForm = (event) => {
   // captures input from state and turns it into a line of poetry
-  // passes line up to Game
-  // Game passes line down to Recent Submission and Final Poem
-  // reset state?
   event.preventDefault();
   let line = '';
   Object.keys(this.state).forEach((key) => {
     line += ' ' + this.state[key];
   })
+  // addLine passes line up to Game; Game passes line down to Recent Submission and Final Poem
   this.addLine(line)
+  // increment player number
   this.playerNumber ++;
+  // reset state so form is back to placeholders for next player
+  this.setState({
+    adjective1: '',
+    noun1: '', 
+    adverb: '', 
+    verb: '', 
+    adjective2: '', 
+    noun2: '',
+  })
 }
 
   render() {
@@ -47,48 +55,60 @@ onSubmitForm = (event) => {
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{this.playerNumber}</h3>
 
-        <form className="PlayerSubmissionForm__form" 
-        onSubmit={this.onSubmitForm}>
+        <form className="PlayerSubmissionForm__form">
           <div className="PlayerSubmissionForm__poem-inputs">
 
           {/* If field is empty, placeholder name appears. Otherwise, letters that the player has entered appear. */}
+          {/* Refactoring input fields to incorporate resetting state pattern we covered in class */}
             The
             <input
-              onChange={(event) => {this.updateWord('adjective1', event)}}
+              name="adjective1"
+              onChange={this.updateWord}
               placeholder={this.state.adjective1 === '' ? "adjective" : this.state.adjective1}
+              value={this.state.adjective1}
               type="text" />
 
             <input
-              onChange={(event) => {this.updateWord('noun1', event)}}
+              name="noun1"
+              onChange={this.updateWord}
               placeholder={this.state.noun1 === '' ? "noun" : this.state.noun1}
+              value={this.state.noun1}
               type="text" />
 
             <input
-              onChange={(event) => {this.updateWord('adverb', event)}}
+              name="adverb"
+              onChange={this.updateWord}
               placeholder={this.state.adverb === '' ? "adverb" : this.state.adverb}
+              value={this.state.adverb}
               type="text" />
 
             <input
-              onChange={(event) => {this.updateWord('verb', event)}}
+              name="verb"
+              onChange={this.updateWord}
               placeholder={this.state.verb === '' ? "verb" : this.state.verb}
+              value={this.state.verb}
               type="text" />
 
             the
 
             <input
-              onChange={(event) => {this.updateWord('adjective2', event)}}
+              name="adjective2"
+              onChange={this.updateWord}
               placeholder={this.state.adjective2 === '' ? "adjective" : this.state.adjective2}
+              value={this.state.adjective2}
               type="text" />
 
             <input
-              onChange={(event) => {this.updateWord('noun2', event)}}
+              name="noun2"
+              onChange={this.updateWord}
               placeholder={this.state.noun2 === '' ? "noun" : this.state.noun2}
+              value={this.state.noun2}
               type="text" />
 
           </div>
 
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input onClick={this.onSubmitForm} type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
           </div>
         </form>
       </div>
--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -5,7 +5,7 @@ const RecentSubmission = (props) => {
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      {/* Most recent submission comes from Game, which receives the line of poetry from User Submission Form */}
+      {/* Most recent submission comes from Game via addLine, which holds the line of poetry from User Submission Form */}
       <p className="RecentSubmission__submission">{ props.recentSubmission  }</p>
     </div>
   );
