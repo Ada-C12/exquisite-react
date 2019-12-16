7507849a897f14d3c7aeb2a5202eadc5db91be04
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-12-15 17:53:47 -0800

    deleted comment

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -30,24 +30,7 @@ const FinalPoem = (props) => {
     </div>
     )}
   
-  
-// console.log(props.poem)
 
-//   return (
-//     <div className="FinalPoem">
-//       <section className="FinalPoem__poem">
-//         <h3>Final Poem</h3>
-      
-//       {finishedPoem}
-      
-//       </section>
-
-//       <div className="FinalPoem__reveal-btn-container">
-
-//         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn FinalPoem__reveal-btn:hover" />
-//       </div>
-//     </div>
-//   );
 }
 
 export default FinalPoem;
